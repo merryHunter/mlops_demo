d17fd42f881cc9774451f38a5fbae8bf8033fe24
Ivan Chernukha <chernukha.ivan@gmail.com>
2025-05-27 00:52:56 +0200

    parametrized docker img

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Set default tag if not provided
+IMAGE_TAG=${IMAGE_TAG:-"25.05.1"}
+
 cd data_prep/
-minikube image build -t alameda_data_prep:25.05.3 .
+minikube image build -t alameda_data_prep:${IMAGE_TAG} .
 cd ../model_training/
-minikube image build -t alameda_model_training:25.05.3 .
+minikube image build -t alameda_model_training:${IMAGE_TAG} .
 cd ../model_monitoring/
-minikube image build -t alameda_model_monitoring:25.05.1 .
+minikube image build -t alameda_model_monitoring:${IMAGE_TAG} .
